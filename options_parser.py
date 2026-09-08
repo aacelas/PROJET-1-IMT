c1b5f79e74e4cb6e0f49320e042c2dd44bc6dc27
daumariecolin-cheeFLame <daumarie.colin@gmail.com>
2026-09-08 10:15:27 +0200

    Change nargs from '*' to '+' for details argument

--- options_parser.py
+++ options_parser.py
@@ -9,12 +9,12 @@ def create_parser():
   
   #ADD SUBPARSER / file.txt add details
   parser_add = subparsers.add_parser("add", help="Add a new task")
-  parser_add.add_argument("details", nargs='*', default='No details', help="Details of the task to add")
+  parser_add.add_argument("details", nargs='+', default='No details', help="Details of the task to add")
   
   #MODIFY SUBPARSER / file.txt modify id details
   parser_modify = subparsers.add_parser("modify", help="Modify an existing task")
   parser_modify.add_argument("id", type=int, help="The id of the task to modify")
-  parser_modify.add_argument("details", nargs='*', default='No details', help="New details of the task")
+  parser_modify.add_argument("details", nargs='+', default='No details', help="New details of the task")
   
   #RM SUBPARSER / file.txt rm id
   parser_rm = subparsers.add_parser("rm", help="Remove an existing task")
