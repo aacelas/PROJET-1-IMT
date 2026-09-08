84fe575fdb429f5f8f9e9b2223fa88350152c0df
daumariecolin-cheeFLame <daumarie.colin@gmail.com>
2026-09-08 11:29:16 +0200

    Update commands.py

--- commands.py
+++ commands.py
@@ -11,20 +11,31 @@ def find_line_by_id(file, id):
         print(f"The file {file} was not found")
         return -1
 
+def find_first_missing_id(file):
+    """Return the first missing id in the file, or 1 if the file is empty."""
+    try:
+        with open(file, 'r') as f:
+            lines = f.readlines()
+            valid_lines = [l.strip() for l in lines if l.strip() != '']
+            if len(valid_lines) == 0:
+                return 1
+            for i in range(len(valid_lines)-1):
+                current_line = valid_lines[i]
+                next_line = valid_lines[i+1]
+                current_id = int(current_line.split(' | ')[0])
+                next_id = int(next_line.split(' | ')[0])
+                if next_id != current_id + 1:
+                    return current_id + 1
+            return int(valid_lines[-1].split(' | ')[0]) + 1
+    except FileNotFoundError:
+        print(f"The file {file} was not found")
+
 
 def add(file, details): 
     """Add a new task with the given details to the file."""
     try:
         with open(file, 'r') as f:
-            lines = f.readlines()
-            #On garde les lignes non vides pour déterminer le prochain id
-            valid_lines = [l.strip() for l in lines if l.strip() != '']
-            if len(valid_lines) == 0:
-                id = 1
-            else:
-                derniere_ligne = lines[-1].strip()
-                str_id = derniere_ligne.split(' | ')[0]
-                id = int(str_id) + 1
+            id = find_first_missing_id(file)
         with open(file, 'a') as f:
             f.write(f'{id}' + ' | ' + ' '.join(details) + '\n')
     except FileNotFoundError:
@@ -36,7 +47,7 @@ def modify(file, id, details):
 
 def rm(file, id):
     "TODO"
-    None 
+    None
 
 def show(file):
     """Show all tasks in the file."""
