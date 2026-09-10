b8d4b6bcc1db903cebc67928e2b19516cd431be8
aacelas <aacelas@unal.edu.co>
2026-09-10 10:39:40 +0200

    multiples labels fix modify

--- options_parser.py
+++ options_parser.py
@@ -21,7 +21,7 @@ def create_parser():
     parser_modify.add_argument("id", type=int, help="The id of the task to modify")
     parser_modify.add_argument("details", nargs='+', default='No details', help="New details of the task")
     parser_modify.add_argument(
-    "--label",
+    "--label", nargs='+',
     help="New label of the task, the label must be one of the following: shopping, sport, homework, administrative, meetings"
 )
     
